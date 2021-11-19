67c8ee1d6c751a96935082eb5671ccd10724eb62
Iman <imanshahmar@gmail.com>
2021-11-19 14:59:33 +0100

    wrote some basic classes for auto and regular cars. Feel free to change

---
+++ tasks/library.py
@@ -0,0 +1,38 @@
+class RegularCar:
+    def __init__(self,position,velocity,area,safe_distance,horse_power):
+        self.position = position
+        self.velocity = velocity
+        self.area = area
+        self.safe_distance = safe_distance
+        self.horse_power = horse_power
+
+    def __str__(self):
+        """
+        stringify card attributes
+        :return: string of cards attributes
+        """
+        return "RegularCar"
+
+
+    def update_postion_velocity(self):
+        pass
+
+
+
+class AutonomousCar:
+    def __init__(self,position,velocity,area,safe_distance,horse_power):
+        self.position = position
+        self.velocity = velocity
+        self.area = area
+        self.safe_distance = safe_distance
+        self.horse_power = horse_power
+    
+    def __str__(self):
+        """
+        stringify card attributes
+        :return: string of cards attributes
+        """
+        return "AutonomousCar"
+
+    def update_postion_velocity(self):
+        pass
