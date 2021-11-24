e541e63a047c56d57797208e51cae62969922a91
alozada10 <a.lozada10@uniandes.edu.co>
2021-11-24 19:40:57 +0100

    axis info added

--- tasks/initialize.py
+++ tasks/initialize.py
@@ -1,36 +1,38 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def initialize(sizeOfPlane,
                axis,
                roadPrecentage):
     initialPosition = []
-    r_x = (sizeOfPlane+(2*np.random.rand()-1)*np.random.rand()*sizeOfPlane*roadPrecentage)/2
-    r_y = (sizeOfPlane+(2*np.random.rand()-1)*np.random.rand()*sizeOfPlane*roadPrecentage)/2
+    r_x = (sizeOfPlane + (2 * np.random.rand() - 1) * np.random.rand() * sizeOfPlane * roadPrecentage) / 2
+    r_y = (sizeOfPlane + (2 * np.random.rand() - 1) * np.random.rand() * sizeOfPlane * roadPrecentage) / 2
     for velocity in axis:
         if velocity == 'x':
-            position = [np.random.rand()*sizeOfPlane,r_x]
+            position = [np.random.rand() * sizeOfPlane, r_x, 0]
             initialPosition.append(position)
         elif velocity == 'y':
-            position = [r_y,np.random.rand() * sizeOfPlane]
+            position = [r_y, np.random.rand() * sizeOfPlane, 1]
             initialPosition.append(position)
     return np.asarray(initialPosition)
 
-#Test
-procent = 0.2 # how wide the width of the road is
-road = 200 #plane size with a height
-v0 = ['x','y','x']
-x0 = initialize(road,v0,procent)
 
-plt.scatter(x0[:,0],x0[:,1])
+# Test
+procent = 0.2  # how wide the width of the road is
+road = 200  # plane size with a height
+v0 = ['x', 'y', 'x', 'x']
+x0 = initialize(road, v0, procent)
+
+plt.scatter(x0[:, 0], x0[:, 1])
 plt.xlim(0, 200)
 plt.ylim(0, 200)
-x1, y1 = [(road-procent*road)/2, (road-procent*road)/2], [0, 200]
-x2, y2 = [(road+procent*road)/2, (road+procent*road)/2], [0, 200]
-x3, y3 = [0, 200], [(road-procent*road)/2, (road-procent*road)/2]
-x4, y4 = [0, 200], [(road+procent*road)/2, (road+procent*road)/2]
+x1, y1 = [(road - procent * road) / 2, (road - procent * road) / 2], [0, 200]
+x2, y2 = [(road + procent * road) / 2, (road + procent * road) / 2], [0, 200]
+x3, y3 = [0, 200], [(road - procent * road) / 2, (road - procent * road) / 2]
+x4, y4 = [0, 200], [(road + procent * road) / 2, (road + procent * road) / 2]
 plt.plot(x1, y1, 'g')
-plt.plot(x2,y2,'g')
+plt.plot(x2, y2, 'g')
 plt.plot(x3, y3, 'g')
-plt.plot( x4, y4, 'g')
+plt.plot(x4, y4, 'g')
 plt.show()
